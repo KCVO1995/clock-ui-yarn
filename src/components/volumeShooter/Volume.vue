0e14eff42a0d5419f816dd53d77c5f6c0c5ed97d
kcvo <943514589@qq.com>
2020-07-14 11:52:27 +0800

    Volume 实现 volume 转动和特效

---
+++ src/components/volumeShooter/Volume.vue
@@ -0,0 +1,44 @@
+<template>
+    <svg class="icon" id="clock-volume" aria-hidden="true"
+         :style="{transform: `rotate(-${rotate}deg)`, width: size + 'px', height: size + 'px'}">
+        <defs>
+            <mask id="Mask">
+                <use :xlink:href="`#i-volume`"/>
+            </mask>
+        </defs>
+        <use :xlink:href="`#i-volume`"/>
+        <circle :cy="size / 2" :r="radius" :fill="color" mask="url(#Mask)"/>
+    </svg>
+</template>
+
+<script>
+  export default {
+    name: "Volume",
+    props: {
+      radius: {
+        type: Number,
+        required: true
+      },
+      rotate: {
+        type: Number,
+        required: true
+      },
+      color: {
+        type: String,
+        required: true
+      },
+      size: {
+        type: Number,
+        required: true,
+      }
+    }
+  }
+</script>
+
+<style scoped>
+    .icon {
+        vertical-align: -0.15em;
+        fill: #999;
+        overflow: hidden;
+    }
+</style>
--- src/components/volumeShooter/VolumeShooter.vue
+++ src/components/volumeShooter/VolumeShooter.vue
@@ -1,32 +1,70 @@
 <template>
-  <div class="global">
-    <svg class="icon" aria-hidden="true">
-      <defs>
-        <mask id="Mask">
-          <use :xlink:href="`#i-volume`"/>
-        </mask>
-      </defs>
-      <use :xlink:href="`#i-volume`"/>
-      <circle cy="25" r="30" fill="pink" mask="url(#Mask)"/>
-    </svg>
-  </div>
+    <div class="global" @mousedown="toggle" @mouseup="toggle">
+        <Volume :radius="volumeRadius" :color="volumeColor" :rotate="volumeRotate" :size="volumeWidth" />
+    </div>
 </template>
 
 <script>
-  export default {}
+  import Volume from "./Volume";
+
+  export default {
+    props: {
+      volumeColor: {
+        type: String,
+        default: 'red'
+      },
+      volumeSize: {
+        type: String,
+        default: 'big',
+        validator: function (value) { return ['small', 'normal', 'big'].indexOf(value) !== -1 }
+      }
+    },
+    computed: {
+      volumeWidth() { return this.sizeToWidth[this.volumeSize] }
+    },
+    watch: {
+      increasing(val) { this.setVolumeRotate(val) }
+    },
+    data() {
+      return {
+        volumeRadius: 0,
+        volumeRotate: 0,
+        volumeSpeed: 1,
+        sizeToWidth: {
+          small: 20,
+          normal: 35,
+          big: 50
+        },
+        increasing: false
+      }
+    },
+    methods: {
+      toggle(e) {
+        let clickVolume = false
+        e.path.forEach(node => {
+          if (node.id === 'clock-volume') clickVolume = true
+        })
+        if (!clickVolume) return
+        this.increasing = !this.increasing
+      },
+      setVolumeRotate(signal) {
+        this.volumeRotate = signal ? this.volumeRotate + this.volumeSpeed : this.volumeRotate - this.volumeSpeed
+        const animation = window.requestAnimationFrame(() => {
+          this.setVolumeRotate(signal)
+          this.setVolumeRadius()
+        })
+        if (this.increasing && this.volumeRotate === 45 || !this.increasing && this.volumeRotate === 0 || signal !== this.increasing) window.cancelAnimationFrame(animation)
+      },
+      setVolumeRadius() { this.volumeRadius = this.volumeRotate / 45 * this.volumeWidth }
+    },
+    components: {Volume},
+  }
 </script>
 
 <style lang='scss' scoped>
-  .global {
-    border: 1px solid red;
-    padding: 50px;
-    .icon {
-      width: 50px;
-      height: 50px;
-      vertical-align: -0.15em;
-      fill: #999;
-      overflow: hidden;
+    .global {
+        border: 1px solid red;
+        padding: 50px;
     }
-  }
 
 </style>
