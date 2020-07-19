1adb4151b469d8fecb9a463b138e8492264b4ffa
KCVO1995 <943514589@qq.com>
2020-07-19 10:12:45 +0800

    commit for meger

---
+++ src/components/volumeShooter/Volume.vue
@@ -0,0 +1,42 @@
+<template>
+    <svg class="icon" aria-hidden="true" :style="{transform: `rotate(-${rotate}deg)`}">
+      <defs>
+        <mask id="Mask">
+          <use :xlink:href="`#i-volume`"/>
+        </mask>
+      </defs>
+      <use :xlink:href="`#i-volume`"/>
+      <circle cy="25" :r="radius" :fill="fillColor" mask="url(#Mask)" />
+    </svg>
+</template>
+
+<script>
+  export default {
+    props: {
+      radius: {
+        type: Number,
+        required: true
+      },
+      rotate: {
+        type: Number,
+        default: 0
+      },
+      fillColor: {
+        type: String,
+        required: true
+      }
+    }
+
+  }
+
+</script>
+
+<style lang='scss' scoped>
+  .icon {
+    width: 50px;
+    height: 50px;
+    vertical-align: -0.15em;
+    fill: #999;
+    overflow: hidden;
+  }
+</style>
--- src/components/volumeShooter/VolumeShooter.vue
+++ src/components/volumeShooter/VolumeShooter.vue
@@ -1,32 +1,64 @@
 <template>
-  <div class="global">
-    <svg class="icon" aria-hidden="true">
-      <defs>
-        <mask id="Mask">
-          <use :xlink:href="`#i-volume`"/>
-        </mask>
-      </defs>
-      <use :xlink:href="`#i-volume`"/>
-      <circle cy="25" r="30" fill="pink" mask="url(#Mask)"/>
-    </svg>
+  <div class="global" @mousedown="toggle" @click="toggle">
+    <Volume id="clock-volume" :rotate="volumeRotate" :radius="volumeRadius" :fill-color="volumeColor"/>
   </div>
 </template>
 
 <script>
-  export default {}
+  import Volume from './Volume'
+  export default {
+    props: {
+      volumeColor: {
+        type: String,
+        default: 'pink'
+      }
+    },
+    data() {
+      return {
+        volumeRadius: 30,
+        volumeRotate: 0,
+        volumeSpeed: 1,
+        increasing: false
+      }
+    },
+    watch: {
+      volumeRotate() {
+
+      }
+    },
+    methods: {
+      toggle(e) {
+        let volumeTarget = false
+        e.path.forEach(node => { if(node.id === 'clock-volume') volumeTarget = true })
+        if(!volumeTarget) return
+        this.increasing = !this.increasing
+        console.log('increasing', this.increasing)
+        if(this.increasing) {
+          this.setVolume('+')
+        } else {
+          this.setVolume('-')
+
+        }
+      },
+      setVolume(target) {
+        console.log('TARGET',target)
+        this.volumeRotate = target ? this.volumeRotate + this.volumeSpeed : this.volumeRotate - this.volumeSpeed
+        window.requestAnimationFrame(() => {
+            if(this.volumeRotate < 45) this.setVolume(target)
+        })
+      }
+    },
+    components: {
+      Volume
+    }
+  }
 </script>
 
 <style lang='scss' scoped>
   .global {
     border: 1px solid red;
     padding: 50px;
-    .icon {
-      width: 50px;
-      height: 50px;
-      vertical-align: -0.15em;
-      fill: #999;
-      overflow: hidden;
-    }
+
   }
 
 </style>
